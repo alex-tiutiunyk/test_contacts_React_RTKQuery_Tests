620266f5576d60206cabe4dad50ac37f68513ac6
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2024-11-14 18:26:05 +0200

    deleted json-server. deploy server on Render. prepare simple test for api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const URL = 'https://test-api-json-server.onrender.com/contacts';
+
+  const [data, getGata] = useState([]);
+
+  function fethData() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        getGata(data)
+        console.log(data)
+      })
+  }
+
+  useEffect(() => {
+    fethData()
+  }, [])
+
+  const addPost = async () => {
+    await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({
+        id: new Date().toISOString(),
+        title: 'New item'
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then(res => res.json())
+      .then(post => console.log(post))
+    fethData()
+  }
 
   return (
     <>
@@ -21,6 +53,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <br/>
+        <br/>
+        <button onClick={addPost}>add</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
